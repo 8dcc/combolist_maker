3a79666688ab57ddc6495b57b15b651905718481
ravi0li <mikel.veg777@gmail.com>
2021-06-15 10:36:25 +0200

    v1.2

--- combo_maker.py
+++ combo_maker.py
@@ -11,22 +11,35 @@ except KeyboardInterrupt:
 	print(" [!] Detected Ctrl+C. Exiting...")
 	exit(1)
 
+hash_dict = {}
+
+def make_dict():
+	with open(hash_pass, "r") as file4dict:
+		while True:
+			line1 = file4dict.readline()
+			if not line1:
+				break
+			hash_from_file = line1.split(":")[0]
+			text_from_file = line1.split(":")[1].strip()
+			hash_dict[hash_from_file] = text_from_file
+
 def main():
-	with open(hash_pass, "r") as r:
-		for line_old in r:
-			hash_from_file = line_old.split(":")[0]
-			text_from_file = line_old.split(":")[1]
-			with open(string_hash, "rt") as string_hash_reader:
-				for string_hash_reader_line in string_hash_reader:
-					if hash_from_file in string_hash_reader_line:
-						replaced_line = string_hash_reader_line.replace(hash_from_file, text_from_file)
-						with open("temp.txt", "a") as result_file_new:
-							result_file_new.write(replaced_line)
+	make_dict()
+	with open(string_hash, "rt", encoding="utf8") as string_hash_reader:
+		while True:
+			line2 = string_hash_reader.readline()
+			if not line2:
+				break
+			hash_from_file2 = line2.split(":")[1]
+			if hash_from_file2 in hash_dict:
+				replaced_line = line2.replace(hash_from_file2, hash_dict[hash_from_file2])
+				with open("temp.txt", "at", encoding="utf8") as result_file_new:
+					result_file_new.write(replaced_line)
 	print(" [+] Done! Removing empty lines...")
-	with open("temp.txt") as infile, open("results.txt", 'w') as outfile:
-		for line in infile:
-			if not line.strip(): continue
-			outfile.write(line)
+	# with open("temp.txt") as infile, open("results.txt", 'w') as outfile:
+	# 	for line in infile:
+	# 		if not line.strip(): continue
+	# 		outfile.write(line)
 	print(" [+] All done!")
 
 main()
